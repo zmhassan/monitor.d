de6756fafc5ff49fd7e2739eac4c76446a3b1ea8
zmhassan <zak.hassan1010@gmail.com>
2016-04-05 21:32:06 -0400

    Adding core api

---
+++ microservices/core-api/src/main/java/com/bzcareer/monitord/core/services/MondCoreServicesApplication.java
@@ -0,0 +1,12 @@
+package com.bzcareer.monitord.core.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MondCoreServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MondCoreServicesApplication.class, args);
+	}
+}
---
+++ microservices/core-api/src/test/java/com/bzcareer/monitord/core/services/MondCoreServicesApplicationTests.java
@@ -0,0 +1,18 @@
+package com.bzcareer.monitord.core.services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MondCoreServicesApplication.class)
+@WebAppConfiguration
+public class MondCoreServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
