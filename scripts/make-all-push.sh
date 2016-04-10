958f10a980db75c3e4a54632c1e068c2072445bc
zmhassan <zak.hassan1010@gmail.com>
2016-04-09 23:57:34 -0400

    adding build files and docker file for backend

---
+++ scripts/make-all-push.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+VERSION="1.0.0-SNAPSHOT"
+AUTHOR="Zak Hassan <zak.hassan@redhat.com>"
+
+
+echo '   __  ___          _ __             ___ '
+echo '  /  |/  /__  ___  (_) /____  ____  / _ \'
+echo ' / /|_/ / _ \/ _ \/ / __/ _ \/ __/ / // /'
+echo '/_/  /_/\___/_//_/_/\__/\___/_/   /____/ '
+
+echo   " Building Docker File For Backend "                                         
+docker   build   -t  bzcareer/monitord-core-services  ../microservices/core-api
+docker   build   -t  bzcareer/monitord  ../web
+# Pushing containers up to docker hub
+docker push bzcareer/monitord
+docker push bzcareer/monitord-core-services
