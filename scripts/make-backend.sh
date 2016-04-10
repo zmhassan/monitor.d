1e6e6c1956c3ef859d37db75ee9a367354a6bb5e
zmhassan <zak.hassan1010@gmail.com>
2016-04-10 00:45:00 -0400

    Finished creating build pusher files

--- scripts/make-backend.sh
+++ scripts/make-backend.sh
@@ -9,5 +9,5 @@ echo ' / /|_/ / _ \/ _ \/ / __/ _ \/ __/ / // /'
 echo '/_/  /_/\___/_//_/_/\__/\___/_/   /____/ '
 
 echo   " Building Docker File For Backend "                                         
-docker   build   -t  bzcareer/monitord  ../microservices/core-api
+docker   build   -t  bzcareer/monitord-core-api  ../microservices/core-api
 
