51101ed7d669519a4651f5e80b729de68e9c04a2
zmhassan <zak.hassan1010@gmail.com>
2016-04-12 19:36:12 -0400

    Fixed issues with start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -2,5 +2,6 @@
 #MONGO_IP=$(echo $DOCKER_HOST | awk -F'[:  /]+' '{ print $2 }')
 #SPRING_MONGO_IP="mongodb://":$MONGO_IP:":27017/mydb"
 #export HOST_MONGO_IP="mongo "$MONGO_IP
-
+export MON_IP=$(docker-machine ip)
+export INIT_MONGO_IP="spring.data.mongodb.uri=mongodb://$MON_IP:27017/mydb"
 docker-compose -f ../docker-compose.yml  up -d
