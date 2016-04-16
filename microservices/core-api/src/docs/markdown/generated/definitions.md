476f1a3f28d2587ca1e7c8ceeb1ab3c499fc81d4
zmhassan <zak.hassan1010@gmail.com>
2016-04-16 17:51:54 -0400

    Data on cloud admin page is feed from mongoDB removing stub ui text

---
+++ microservices/core-api/src/docs/markdown/generated/definitions.md
@@ -0,0 +1,47 @@
+## Definitions
+### JobDAO
+|Name|Description|Required|Schema|Default|
+|----|----|----|----|----|
+|end||false|string||
+|id||false|string||
+|name||false|string||
+|start||false|string||
+|status||false|string||
+|type||false|string||
+
+
+### LogDAO
+|Name|Description|Required|Schema|Default|
+|----|----|----|----|----|
+|appName||false|string||
+|end||false|string||
+|id||false|string||
+|start||false|string||
+
+
+### NodeDAO
+|Name|Description|Required|Schema|Default|
+|----|----|----|----|----|
+|id||false|string||
+|serverIp||false|string||
+|serverName||false|string||
+
+
+### NotificationDAO
+|Name|Description|Required|Schema|Default|
+|----|----|----|----|----|
+|from||false|string||
+|id||false|string||
+|message||false|string||
+|notification_type||false|string||
+
+
+### UserDAO
+|Name|Description|Required|Schema|Default|
+|----|----|----|----|----|
+|id||false|string||
+|password||false|string||
+|team||false|string||
+|username||false|string||
+
+
---
+++ microservices/core-api/src/test/java/com/bzcareer/monitord/core/services/DocGenTest.java
@@ -0,0 +1,56 @@
+package com.bzcareer.monitord.core.services;
+
+import javax.ws.rs.core.MediaType;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.bzcareer.monitord.core.config.MondCoreServicesApplication;
+
+import io.github.robwin.markup.builder.MarkupLanguage;
+import springfox.documentation.staticdocs.Swagger2MarkupResultHandler;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MondCoreServicesApplication.class)
+public class DocGenTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+    }
+
+    @Test
+    public void convertSwaggerToAsciiDoc() throws Exception {
+        this.mockMvc.perform(get("/v2/api-docs?group=coreapi")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(Swagger2MarkupResultHandler
+                    .outputDirectory("src/docs/asciidoc/generated").build())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void convertSwaggerToMarkdown() throws Exception {
+        this.mockMvc.perform(get("/v2/api-docs?group=coreapi")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(Swagger2MarkupResultHandler.outputDirectory("src/docs/markdown/generated")
+                    .withMarkupLanguage(MarkupLanguage.MARKDOWN).build())
+                .andExpect(status().isOk());
+    }
+}
