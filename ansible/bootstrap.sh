eb076babe02ff0c89a7154a2b82770bf21a55f49
zmhassan <zak.hassan1010@gmail.com>
2016-05-07 20:10:55 -0400

    Adding ansible changes to optimize platform

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -7,13 +7,5 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 #sudo yum group install "Development Tools" -y
 sudo yum install epel-release -y
 sudo yum install ansible -y
+
  
-
-
-cat >> /etc/hosts <<EOL
-
-192.168.33.40 hadoopmaster
-192.168.33.41 hadoopdata1
-192.168.33.42 hadoopdata2
-
-EOL
