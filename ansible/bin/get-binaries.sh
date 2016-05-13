d90a063795ef0153479e5e3060369859504514ef
zak hassan <zhassan@zhassan.csb>
2016-05-12 22:04:43 -0500

    making sure we have support for fedora 23 and libvirt vagrant setup

---
+++ ansible/bin/get-binaries.sh
@@ -0,0 +1,6 @@
+
+
+#curl -o ../roles/hadoop/files/hadoop-2.7.1.tar.gz  http://apache.mirror.iweb.ca/hadoop/common/hadoop-2.7.1/hadoop-2.7.1.tar.gz
+curl -o ../roles/pig/files/pig-0.15.0.tar.gz    http://apache.mirror.gtcomm.net/pig/pig-0.15.0/pig-0.15.0.tar.gz
+curl -o ../roles/hive/files/apache-hive-2.0.0-bin.tar.gz   http://apache.mirror.gtcomm.net/hive/hive-2.0.0/apache-hive-2.0.0-bin.tar.gz
+curl -o ../roles/spark/files/spark-1.6.1-bin-hadoop2.6.tgz   http://d3kbcqa49mib13.cloudfront.net/spark-1.6.1-bin-hadoop2.6.tgz
