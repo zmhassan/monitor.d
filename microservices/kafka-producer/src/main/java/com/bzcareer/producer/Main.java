82f9a0903dcc4f731e54f2aa879b2f568feee5d2
zmhassan <zak.hassan1010@gmail.com>
2016-06-25 15:37:10 -0400

    more optimizations to utils

--- microservices/kafka-producer/src/main/java/com/bzcareer/producer/Main.java
+++ microservices/kafka-producer/src/main/java/com/bzcareer/producer/Main.java
@@ -1,14 +1,24 @@
 package com.bzcareer.producer;
 
+import java.util.concurrent.ExecutionException;
+
 public class Main {
 
 	public static void main(String[] args) {
 		KafkaMessenger messenger = new KafkaMessenger("localhost:9092");
-		messenger.send("logtest", "apple");
-		messenger.send("logtest", "orange");
-		messenger.send("logtest", "watermelon");
-		messenger.send("logtest", "grape");
-		messenger.close();
+
+		try {
+
+			messenger.send("logtest", "apple").get();
+			messenger.send("logtest", "orange").get();
+			messenger.send("logtest", "watermelon").get();
+			messenger.send("logtest", "grape").get();
+
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		} finally {
+			messenger.close();
+		}
 	}
 
 }
