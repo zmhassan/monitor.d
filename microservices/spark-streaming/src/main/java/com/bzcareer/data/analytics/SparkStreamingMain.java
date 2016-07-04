e045d17898c67c36b3d1aa40d905aaf42859ec75
Zak Hassan <zak.hassan1010@gmail.com>
2016-07-04 16:06:33 -0400

    Removing build failure

--- microservices/spark-streaming/src/main/java/com/bzcareer/data/analytics/SparkStreamingMain.java
+++ microservices/spark-streaming/src/main/java/com/bzcareer/data/analytics/SparkStreamingMain.java
@@ -74,7 +74,6 @@ public class SparkStreamingMain {
 			JsonReader reader = new JsonReader(new StringReader(stripOutQuotes));
 			reader.setLenient(true);
 			logs = gson.fromJson(reader, WebAccessRecords.class);
-			saveToCassandra(logs);
 		} catch (Exception e) {
 			System.err.println("Invalid String:"+ items.substring(1,items.length()-2));
 			System.err.println(e);
